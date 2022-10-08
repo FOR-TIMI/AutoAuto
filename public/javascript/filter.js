15d6eabb66b4448c40e7c902ec68c50a4375d45b
Adam Gaboury <adam.gaboury@hotmail.com>
2022-10-08 17:43:45 -0400

    handlebars file structure added

---
+++ public/javascript/filter.js
@@ -0,0 +1,25 @@
+async function filterClickHandler(event) {
+    event.preventDefault();
+  
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch('/api/searchoptions/filterbtn', {
+      method: 'PUT',
+      body: JSON.stringify({
+        post_id: id
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.filter-btn').addEventListener('click', filterClickHandler);
+  
