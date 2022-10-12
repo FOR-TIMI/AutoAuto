e0f078d652e5e7adfba4dfafbf7003f9ca3fda82
FOR-TIMI <timiplayskeys@gmail.com>
2022-10-12 04:16:58 -0400

    added render to login and register

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -3,3 +3,11 @@ const {Vehicle} = require('../models');
 module.exports.renderHomePage = async(req,res) => {
     res.render('cars/index')
 }
+
+module.exports.renderLoginPage = async(req,res) => {
+    res.render('user/login')
+}
+
+module.exports.renderRegisterPage = async(req,res) => {
+    res.render('user/register')
+}
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -5,4 +5,10 @@ const homeController = require('../controllers/home-controller')
 router.route('/')
       .get(homeController.renderHomePage)
 
+router.route('/login')
+      .get(homeController.renderLoginPage)
+
+router.route('/register')
+      .get(homeController.renderRegisterPage)
+
 module.exports =  router
